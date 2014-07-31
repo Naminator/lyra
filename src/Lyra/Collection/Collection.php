f3424722be2dbb99618ab24bd3d9101134e5575e
Lachezar Tsochev <ltsochev@live.com>
2014-07-31 16:44:28 +0300

    Comment and composer updates

--- src/Lyra/Collection/Collection.php
+++ src/Lyra/Collection/Collection.php
@@ -204,11 +204,22 @@ class Collection implements ArrayAccess, ArrayableInterface, Countable, Iterator
 		}, $this->items);
 	}
 
+	/**
+	 * Get the collection of items as JSON.
+	 *
+	 * @param  int  $options
+	 * @return string
+	 */
 	public function toJson($options = 0)
 	{
 		return json_encode($this->toArray(), $options);
 	}
 
+	/**
+	 * Convert the collection to its string representation.
+	 *
+	 * @return string
+	 */
 	public function __toString()
 	{
 		return $this->toJson();
