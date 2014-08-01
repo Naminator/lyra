b3f955c7e20fa2189d24bc9a71873b325c86d8e6
Lachezar Tsochev <ltsochev@live.com>
2014-08-01 18:46:50 +0300

    Response Class

---
+++ src/Lyra/Response.php
@@ -0,0 +1,13 @@
+<?php namespace Lyra;
+
+use Lyra\Collection\Collection;
+use Lyra\Contracts\ArrayableInterface;
+use Lyra\Contracts\JsonableInterface;
+
+class Response implements ArrayableInterface, JsonableInterface
+{
+	public function __construct($data = NULL)
+	{
+
+	}
+}
